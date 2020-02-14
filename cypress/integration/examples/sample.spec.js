b26648b831d9f9e326de04525fdf0be8e0fd7b2d
Sumit Tulsyan <sumittulsyan@Sumits-Macbook-Pro.local>
2020-02-14 11:58:46 +0530

    erffg

--- cypress/integration/examples/sample.spec.js
+++ cypress/integration/examples/sample.spec.js
@@ -1,12 +1,12 @@
-describe('My First Test', function(){
-    it('Does not do much',function(){
-cy.visit('https://builderx.io/')
-cy.contains('Login').click()
+// describe('My First Test', function(){
+//     it('Does not do much',function(){
+// cy.visit('https://builderx.io/')
+// cy.contains('Login').click()
 
 
-cy.contains('Login with email').click()
-cy.get('.action-email')
-  .type('fake@email.com').should('have.value', 'fake@email.com')
+// cy.contains('Login with email').click()
+// cy.get('.action-email')
+//   .type('fake@email.com').should('have.value', 'fake@email.com')
 
-})
-})
+// })
+// })
